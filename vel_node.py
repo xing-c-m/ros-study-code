0965617c6ae39ecab79f31e3fba802bb697b64cf
xing-c-m <2974086593@qq.com>
2025-02-08 19:48:46 +0800

    Add files via upload

---
+++ vel_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__=="__main__":
+    rospy.init_node("vel_node")
+
+    vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
+
+    vel_msg = Twist()
+    vel_msg.angular.z = 0.5
+
+    rate = rospy.Rate(30)
+    while not rospy.is_shutdown():
+        vel_pub.publish(vel_msg)
+        rate.sleep()
