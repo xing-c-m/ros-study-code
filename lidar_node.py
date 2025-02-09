bc8fe85fc9900ae9f043d69f650ae250921b7b85
xing-c-m <2974086593@qq.com>
2025-02-09 21:41:42 +0800

    Add files via upload

--- lidar_node.py
+++ lidar_node.py
@@ -3,11 +3,30 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+count = 0
 
 def LidarCallback(msg):
+    global vel_pub
+    global count
     dist = msg.ranges[180]
     rospy.loginfo("前方测距ranges[180] = %f米",dist)
+
+    if count > 0:
+        count = count-1
+        return
+    
+    vel_cmd = Twist()
+    if dist < 1.5:
+        vel_cmd.angular.z = 0.3
+        count = 50
+    else:
+        vel_cmd.linear.x = 0.05
+    vel_pub.publish(vel_cmd)
+
 if __name__=="__main__":
     rospy.init_node("lidar_node")
     lidar_sub = rospy.Subscriber("/scan",LaserScan,LidarCallback,queue_size=10)
+    vel_pub = rospy.Publisher("/cmd_vel",Twist,queue_size=10)
     rospy.spin()
