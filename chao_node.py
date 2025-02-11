97767e49920c2fe906ee143aeb10b846556b2496
xing-c-m <2974086593@qq.com>
2025-02-11 17:25:43 +0800

    Add files via upload

--- chao_node.py
+++ chao_node.py
@@ -3,18 +3,22 @@
 
 import rospy
 from std_msgs.msg import String
+from qq_msgs.msg import Carry
 
 if __name__=="__main__":
     rospy.init_node("chao_node")
     rospy.logwarn("我的枪去而复返，你的生命有去无回！")
 
-    pub = rospy.Publisher("kuai_shang_che_kai_hei_qun",String,queue_size=10)
+    pub = rospy.Publisher("kuai_shang_che_kai_hei_qun",Carry,queue_size=10)
 
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
         rospy.loginfo("我要开始刷屏了")
-        msg = String()
+        msg = Carry()
+        msg.grade = "王者"
+        msg.star = 50
+        
         msg.data = "国服马超，带飞"
         pub.publish(msg)
         rate.sleep()
--- ma_node.py
+++ ma_node.py
@@ -3,8 +3,10 @@
 
 import rospy
 from std_msgs.msg import String
-
+from qq_msgs.msg import Carry
 def chao_callback(msg):
+    rospy.logwarn(msg.grade)
+    rospy.logwarn(str(msg.star)+"星")
     rospy.loginfo(msg.data)
 
 def yao_callback(msg):
@@ -13,7 +15,7 @@ def yao_callback(msg):
 if __name__=="__main__":
     rospy.init_node("ma_node")
 
-    sub = rospy.Subscriber("kuai_shang_che_kai_hei_qun",String,chao_callback,queue_size=10)
+    sub = rospy.Subscriber("kuai_shang_che_kai_hei_qun",Carry,chao_callback,queue_size=10)
     sub_2 = rospy.Subscriber("ge_ge_dai_wo",String,yao_callback,queue_size=10)
     
     rospy.spin()
