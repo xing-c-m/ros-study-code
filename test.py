45b2196ec69e70626beb79363b16d3eca0a31b75
xing-c-m <2974086593@qq.com>
2025-03-01 11:57:01 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import torch
+import torch.nn.functional as F
 
 x_data = torch.Tensor([[1.0],[2.0],[3.0]])
-y_data = torch.Tensor([[2.0],[4.0],[6.0]])
+y_data = torch.Tensor([[0],[0],[1]])
 
-class LinearModel(torch.nn.Module):
+class LogisticRegressionModel(torch.nn.Module):
     def __init__(self):
-        super(LinearModel,self).__init__()
+        super(LogisticRegressionModel,self).__init__()
         self.linear = torch.nn.Linear(in_features=1,out_features=1)
 
     def forward(self,x):
-        y_pred = self.linear(x)
+        y_pred = F.sigmoid(self.linear(x))
         return y_pred
-model = LinearModel()
+model = LogisticRegressionModel()
 
-criterion = torch.nn.MSELoss(size_average=False)
+criterion = torch.nn.BCELoss(size_average=False)
 optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
 
 for epoch in range(10000):
